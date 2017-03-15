f163d4eff163e406d1d7b65fbb8b3b751bd852f4
kellyydangg <kellydang95@yahoo.com>
2017-03-15 18:30:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kelly/raspberrypi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kelly.raspberrypi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
